9d553aad8dc6c3b4174f18902b0caf2853dde267
DanWarrioR <norkinDanya2006@gmail.com>
2025-07-19 11:21:38 +0200

    Added GlobalConfig

---
+++ Assets/Scripts/Data/GlobalConfig.cs
@@ -0,0 +1,26 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Data
+{
+    public struct GlobalConfigComponent : IComponentData
+    {
+        public int foodCount;
+        public Entity foodPrefab;
+
+        public int playerCount;
+        public Entity playerPrefab;
+        public Entity mainPlayerPrefab;
+    }
+    
+    [CreateAssetMenu(menuName = "Configs/GlobalConfig", order = 0)]
+    public class GlobalConfig : ScriptableObject
+    {
+        public int foodCount;
+        public GameObject foodPrefab;
+        
+        public int playerCount;
+        public GameObject playerPrefab;
+        public GameObject mainPlayerPrefab;
+    }
+}
---
+++ Assets/Scripts/Data/GlobalConfigAuthoring.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Data
+{
+    public class GlobalConfigAuthoring : MonoBehaviour
+    {
+        [SerializeField]
+        private GlobalConfig config;
+        
+        private class GlobalConfigAuthoringBaker : Baker<GlobalConfigAuthoring>
+        {
+            public override void Bake(GlobalConfigAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.None);
+                var config = authoring.config;
+                AddComponent(entity, new GlobalConfigComponent
+                {
+                    foodPrefab = GetEntity(config.foodPrefab, TransformUsageFlags.Dynamic),
+                    foodCount = config.foodCount,
+
+                    playerPrefab = GetEntity(config.playerPrefab, TransformUsageFlags.Dynamic),
+                    mainPlayerPrefab = GetEntity(config.mainPlayerPrefab, TransformUsageFlags.Dynamic),
+                    playerCount = config.playerCount,
+                });
+            }
+        }
+    }
+}
