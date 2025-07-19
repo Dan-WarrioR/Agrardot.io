6aaeb151673bdbde2b49417f730ea92d7fbb3f15
DanWarrioR <norkinDanya2006@gmail.com>
2025-07-19 12:00:21 +0200

    Food Prefab test spawn

---
+++ Assets/Scripts/Features/Spawn/PrefabConfigAuthoring.cs
@@ -0,0 +1,32 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Features.Spawn
+{
+    public struct FoodPrefabComponent : IComponentData
+    {
+        public Entity foodPrefab;
+        public int count;
+    }
+    
+    public class PrefabConfigAuthoring : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject foodPrefab;
+        [SerializeField]
+        private int count = 100;
+        
+        private class PrefabConfigAuthoringBaker : Baker<PrefabConfigAuthoring>
+        {
+            public override void Bake(PrefabConfigAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.None);
+                AddComponent(entity, new FoodPrefabComponent()
+                {
+                    foodPrefab = GetEntity(authoring.foodPrefab, TransformUsageFlags.Dynamic),
+                    count = authoring.count,
+                });
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Features/Spawn/SpawnFoodSystem.cs
@@ -0,0 +1,43 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace Features.Spawn
+{
+    [UpdateInGroup(typeof(GameplaySystemGroup))]
+    public partial struct SpawnFoodSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<FoodPrefabComponent>();
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            state.Enabled = false;
+
+            var config = SystemAPI.GetSingleton<FoodPrefabComponent>();
+            var ecb = new EntityCommandBuffer(Allocator.Temp);
+            var random = new Random();
+            
+            for (int i = 0; i < config.count; i++)
+            {
+                var food = ecb.Instantiate(config.foodPrefab);
+
+                float3 position = new float3(
+                    random.NextFloat(-5f, 5f),
+                    random.NextFloat(-5f, 5f),
+                    0f);
+
+                ecb.SetComponent(food, LocalTransform.FromPosition(position));
+            }
+
+            ecb.Playback(state.EntityManager);
+            ecb.Dispose();
+        }
+    }
+}
