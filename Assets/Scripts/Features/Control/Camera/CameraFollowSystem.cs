fd4d0ed79d5ace7d5c5176597f913a668bcbd611
DanWarrioR <norkinDanya2006@gmail.com>
2025-07-25 10:36:16 +0200

    Rewrited camera control

---
+++ Assets/Scripts/Features/Control/Camera/CameraFollowSystem.cs
@@ -0,0 +1,53 @@
+﻿using Features.Units.Player;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace Features.Control.Camera
+{
+    public partial class CameraFollowSystem : SystemBase
+    {
+        private float3 _targetPosition;
+        
+        protected override void OnCreate()
+        {
+            RequireForUpdate<UserTag>();
+            RequireForUpdate<CameraSettingsComponent>();
+        }
+        
+        protected override void OnUpdate()
+        {
+            if (!SystemAPI.HasSingleton<UserTag>() 
+                || !SystemAPI.HasSingleton<CameraSettingsComponent>())
+            {
+                return;
+            }
+
+            var camera = UnityEngine.Camera.main.transform;
+            var config = SystemAPI.GetSingleton<CameraSettingsComponent>();
+
+            var playerEntity = SystemAPI.GetSingletonEntity<UserTag>();
+            var playerTransform = SystemAPI.GetComponentRO<LocalToWorld>(playerEntity).ValueRO;
+            
+            float3 desiredPosition = playerTransform.Position + config.offset;
+            float3 cameraPosition = camera.position;
+
+            float distanceToPlayer = math.distance(cameraPosition, desiredPosition);
+
+            if (distanceToPlayer > config.followThreshold)
+            {
+                _targetPosition = desiredPosition;
+            }
+
+            float maxStep = config.lerpSpeed * SystemAPI.Time.DeltaTime;
+            float3 direction = _targetPosition - cameraPosition;
+            float distance = math.length(direction);
+            
+            float3 newPosition = math.lerp(cameraPosition, _targetPosition, config.lerpSpeed * SystemAPI.Time.DeltaTime);
+            camera.position = newPosition;
+            
+            //float3 move = direction / distance * maxStep;
+            //camera.position = cameraPosition + move;
+        }
+    }
+}
---
+++ Assets/Scripts/Features/Control/Camera/CameraSettingsAuthoring.cs
@@ -0,0 +1,37 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Features.Control.Camera
+{
+    public struct CameraSettingsComponent : IComponentData
+    {
+        public float3 offset;
+        public float lerpSpeed;
+        public float followThreshold;
+    }
+    
+    public class CameraSettingsAuthoring : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector3 offset = new Vector3(0, 0, -10);
+        [SerializeField]
+        private float lerpSpeed = 5f;
+        [SerializeField]
+        private float followThreshold = 1.5f;
+        
+        private class CameraSettingsAuthoringBaker : Baker<CameraSettingsAuthoring>
+        {
+            public override void Bake(CameraSettingsAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.None);
+                AddComponent(entity, new CameraSettingsComponent
+                {
+                    offset = authoring.offset,
+                    lerpSpeed = authoring.lerpSpeed,
+                    followThreshold = authoring.followThreshold
+                });
+            }
+        }
+    }
+}
