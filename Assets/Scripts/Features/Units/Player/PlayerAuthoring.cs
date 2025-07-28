1fffafc42a197ec7114109afae8cc9725bd65937
DanWarrioR <norkinDanya2006@gmail.com>
2025-07-28 20:34:31 +0200

    Fixed player

--- Assets/Scripts/Features/Units/Player/PlayerAuthoring.cs
+++ Assets/Scripts/Features/Units/Player/PlayerAuthoring.cs
@@ -17,13 +17,17 @@ namespace Features.Units.Player
     public struct MovementComponent : IComponentData
     {
         public float2 velocity;
-        public float speed;
+        public float baseSpeed;
+        public float currentSpeed;
     }
     
     public class PlayerAuthoring : MonoBehaviour
     {
         [SerializeField]
         private float moveSpeed;
+
+        [SerializeField]
+        private bool isUser = false;
         
         private class PlayerAuthoringBaker : Baker<PlayerAuthoring>
         {
@@ -31,12 +35,17 @@ namespace Features.Units.Player
             {
                 var entity = GetEntity(TransformUsageFlags.Dynamic);
                 AddComponent<PlayerTag>(entity);
-                AddComponent<UserTag>(entity);
+
+                if (authoring.isUser)
+                {
+                    AddComponent<UserTag>(entity);
+                }
                 
                 AddComponent(entity, new MovementComponent
                 {
                     velocity = float2.zero,
-                    speed = authoring.moveSpeed,
+                    baseSpeed = authoring.moveSpeed,
+                    currentSpeed = authoring.moveSpeed,
                 });
             }
         }
--- Assets/Scripts/Features/Units/Player/PlayerMovementSystem.cs
+++ Assets/Scripts/Features/Units/Player/PlayerMovementSystem.cs
@@ -1,7 +1,7 @@
-﻿using Unity.Burst;
+﻿using Data;
+using Unity.Burst;
 using Unity.Entities;
 using Unity.Mathematics;
-using Unity.Physics;
 using Unity.Transforms;
 
 namespace Features.Units.Player
@@ -9,17 +9,29 @@ namespace Features.Units.Player
     public partial struct PlayerMovementSystem : ISystem
     {
         [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<GlobalConfigComponent>();
+        }
+        
+        [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
+            var config = SystemAPI.GetSingleton<GlobalConfigComponent>();
+            
             foreach (var (transform, movement) 
                      in SystemAPI.Query<
                          RefRW<LocalTransform>, 
                          RefRO<MovementComponent>>())
             {
                 float2 direction = movement.ValueRO.velocity;
-                float3 delta = new float3(direction.x, direction.y, 0) * movement.ValueRO.speed * SystemAPI.Time.DeltaTime;
+                float3 delta = new float3(direction.x, direction.y, 0) * movement.ValueRO.baseSpeed * SystemAPI.Time.DeltaTime;
+                float3 newPosition = transform.ValueRO.Position + delta;
                 
-                transform.ValueRW.Position += delta;
+                newPosition.x = math.clamp(newPosition.x, config.mapMin.x, config.mapMax.x);
+                newPosition.y = math.clamp(newPosition.y, config.mapMin.y, config.mapMax.y);
+
+                transform.ValueRW.Position = newPosition;
             }
         }
     }
